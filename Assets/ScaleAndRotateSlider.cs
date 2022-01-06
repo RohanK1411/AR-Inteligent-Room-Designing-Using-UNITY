9d01b42d55fc74cfe136ae6ae483d68487af9f0b
Rohan_k1411 <rohankatta1411@gmail.com>
2022-01-06 14:10:17 +0530

    first commit

---
+++ Assets/ScaleAndRotateSlider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleAndRotateSlider : MonoBehaviour
+{
+    
+    private Slider scaleSlider;
+    public float scaleMinValue;
+    public float scaleMaxValue;
+
+
+    void Start()
+    {
+        
+        scaleSlider = GameObject.Find("ScaleSlider").GetComponent<Slider>();
+        scaleSlider.minValue = scaleMinValue;
+        scaleSlider.maxValue = scaleMaxValue;
+
+        scaleSlider.onValueChanged.AddListener(ScaleSliderUpdate);
+
+    }
+
+    void ScaleSliderUpdate(float value)
+    {
+        transform.localScale = new Vector3(value, value, value);
+    }
+
+
+}
